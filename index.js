89415f0e04b3564feaf6f6aade1f3dce45d3f45d
Huzaifa Jadoon <145706459+huzaifajadoon700@users.noreply.github.com>
2023-12-17 19:36:26 +0500

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,13 @@ mongoose.connect(url , {useNewUrlParser: true , useUnifiedTopology: true } )
 .catch(()=>console.log("not connected"));
 
 
-app.use(cors());
+app.use(cors(
+  {
+    origin :["https://deploy.mern.1whq.vercel.app"],
+             methods: ["POST", "GET"],
+    credentials: true
+  }
+));
 app.use(bodyParser.json( { extends:true } ) );
 app.use(bodyParser.urlencoded({extends:true }));
 app.use("/shoes",shoes);
